4be69e9b63bff609d2cb2a3c49a180a1709b6969
Maxim Yakovlev <it.trainee@yandex.ru>
2022-09-20 22:28:38 +0300

    calculating the factorial using a loop

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const factorial = (number) => {
+const factorialRecursion = (number) => {
   if (number === 1) {
     return 1;
   }
   return number * factorial(number - 1);
+};
+
+const factorialLoop = (number) => {
+  let result = 1;
+  for (let i = 2; i <= number; i++) {
+    result *= i;
+  }
+  return result;
 };
